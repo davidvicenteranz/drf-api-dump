dd5e6f04d64c00aca8f8cd9902604c5117dd3dfb
david <dvicente74@gmail.com>
2017-07-18 16:35:53 +0100

    created dist and keywords added

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
     install_requires=[
         'djangorestframework'
     ],
+    keywords='django dumpdata development',
     classifiers=(
         'Framework :: Django',
         'Development Status :: 4 - Beta',
